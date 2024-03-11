5af7c0167f07059fe04105699a3603cda5c4e34d
Kate <knl8@sfu.ca>
2024-03-11 10:38:29 -0700

    Added new Files
    Edit website to be more consistent

--- js/script.js
+++ js/script.js
@@ -1,41 +1,16 @@
-function toggleFullScreen(img) {
-        
-  if (!img.classList.contains('fullscreen')) {
-    // If the image is not in full-screen mode, make it full screen
-    img.classList.add('fullscreen');
-  } else {
-    // If the image is already in full-screen mode, remove the full-screen class
-    img.classList.remove('fullscreen');
-  }
-}
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= itemWidth + padding
+})
+
+next.addEventListener('click',()=>{
+  list.scrollLeft += itemWidth + padding
+})
 
 
-function increaseValue1() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number').value = value;
-}
-
-function decreaseValue1() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number').value = value;
-}
-
-function increaseValue2() {
-  var value = parseInt(document.getElementById('number1').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number1').value = value;
-}
-
-function decreaseValue2() {
-  var value = parseInt(document.getElementById('number1').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number1').value = value;
-}
